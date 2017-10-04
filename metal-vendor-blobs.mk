b0c726deaf93508bff2b6b4b9ecfb7dc71847a06
fire855 <thefire855@gmail.com>
2017-10-04 23:12:07 +0200

    Move everything to vendor

--- metal-vendor-blobs.mk
+++ metal-vendor-blobs.mk
@@ -1,11 +1,3 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ulefone/metal/lib,system/lib)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ulefone/metal/lib64,system/lib64)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ulefone/metal/bin,system/bin)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ulefone/metal/etc,system/etc)
-
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ulefone/metal/usr,system/usr)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ulefone/metal/vendor,system/vendor)
